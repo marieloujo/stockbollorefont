866c259c57262bdd1474dce929b623354b18179b
JAMES Patrick <meto26pat@gmail.com>
2021-04-30 01:36:53 +0100

    add form marque modele and gamme

--- src/app/dashboard/materiel/marque/marque.component.ts
+++ src/app/dashboard/materiel/marque/marque.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {BehaviorService} from '../../../services/common/behavior.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Magasin} from '../../../models/magasin';
+import {MagasinService} from '../../../services/dashboard/magasin.service';
+import {MarqueService} from '../../../services/dashboard/marque.service';
+import {Marque} from '../../../models/marque';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-marque',
@@ -8,12 +14,132 @@ import {BehaviorService} from '../../../services/common/behavior.service';
 })
 export class MarqueComponent implements OnInit {
 
+  validateMagasinForm!: FormGroup;
+
+  marqueList: Marque[];
+
+  indexOfTab: number;
+
+  listOfColumn: any = [];
+
   constructor(
     private behaviorService: BehaviorService,
+    private fb: FormBuilder,
+    private marqueService: MarqueService
   ) { }
 
   ngOnInit(): void {
     this.behaviorService.setBreadcrumbItems(['Accueil', 'Matériel', 'Marque']);
+
+
+    this.makeMarqueForm(null);
+
+    this.list();
+
+    this.listOfColumnHeadeer();
+  }
+
+  makeMarqueForm(magasin: Magasin){
+    this.validateMagasinForm = this.fb.group({
+      id: [magasin != null ? magasin.id : null],
+      libelle: [magasin != null ? magasin.libelle : null,
+        [Validators.required]],
+    });
+  }
+
+  resetMarqueForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateMagasinForm.reset();
+    for (const key in this.validateMagasinForm.controls) {
+      this.validateMagasinForm.controls[key].markAsPristine();
+      this.validateMagasinForm.controls[key].updateValueAndValidity();
+    }
+    this.makeMarqueForm(null);
+    console.log('Tab en cours ==> ', this.indexOfTab);
+  }
+
+  list(): void {
+    this.marqueService.getList().subscribe(
+      (data: any) => {
+        this.marqueList = data;
+        console.log('MagasinList ==>', this.marqueList);
+      },
+      (error: HttpErrorResponse) => {
+        console.log('error getList Magasin ==>', error.message, ' ', error.status, ' ', error.statusText);
+      });
+  }
+
+  listOfColumnHeadeer(){
+    this.listOfColumn = [
+      /*{
+        title: 'Name',
+        compare: null,
+        priority: false
+      },*/
+      {
+        title: 'Libellé',
+        compare: null,
+        priority: false
+      },
+      /*{
+        title: 'Math Score',
+        compare: (a: DataItem, b: DataItem) => a.math - b.math,
+        priority: 2
+      },
+      {
+        title: 'English Score',
+        compare: (a: DataItem, b: DataItem) => a.english - b.english,
+        priority: 1
+      }*/
+    ];
+  }
+
+  submitMarqueForm(): void {
+    for (const i in this.validateMagasinForm.controls) {
+      this.validateMagasinForm.controls[i].markAsDirty();
+      this.validateMagasinForm.controls[i].updateValueAndValidity();
+    }
+
+    if (this.validateMagasinForm.valid) {
+
+      const formData = this.validateMagasinForm.value;
+      if (formData.id == null) {
+        this.marqueService.createMarque(formData).subscribe(
+          (data: any) => {
+            this.marqueList.unshift(data);
+            this.marqueList = [...this.marqueList];
+            this.makeMarqueForm(null);
+
+            console.log('Enregistrement ok');
+            this.indexOfTab = 0;
+
+          },
+          (error: HttpErrorResponse) => {
+            console.log('Enregistrement non ok');
+
+          });
+      } else {
+        const i = this.marqueList.findIndex(p => p.id == formData.id);
+        this.marqueService.updateMarque(formData).subscribe(
+          (data: any) => {
+            this.marqueList[i] = data[0];
+            this.marqueList = [...this.marqueList];
+            this.makeMarqueForm(null);
+
+            console.log('Update ok');
+            this.indexOfTab = 0;
+
+          },
+          (error: HttpErrorResponse) => {
+            console.log('Update non ok');
+          });
+      }
+
+    }
+    else {
+
+    }
+
   }
 
 }
--- src/app/services/dashboard/gamme.service.ts
+++ src/app/services/dashboard/gamme.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Magasin} from '../../models/magasin';
+import {Gamme} from '../../models/gamme';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GammeService {
 
-  constructor() { }
+  url: string = environment.backend +'/gamme'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getList(): Observable<Object> {
+    return this.http.get(`${this.url}/list`);
+  }
+
+  createGamme(gamme: Gamme): Observable<Object> {
+    return this.http.post(`${this.url}/creer-gamme`, gamme);
+  }
+
+  updateGamme(gamme: Gamme): Observable<Object> {
+    return this.http.put(`${this.url}/modifier-gamme`, gamme);
+  }
+
+  deleteGamme(id: number): Observable<Object> {
+    return this.http.delete(`${this.url}/supprimer-gamme/${id}`);
+  }
+
 }
