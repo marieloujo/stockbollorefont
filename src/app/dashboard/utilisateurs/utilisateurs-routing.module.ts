0c3ecb81c96a038575460b21d636637d3041f3ca
JAMES Patrick <meto26pat@gmail.com>
2021-05-07 16:26:33 +0100

    modif emplacement user module

--- src/app/dashboard/utilisateurs/utilisateurs-routing.module.ts
+++ src/app/dashboard/utilisateurs/utilisateurs-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DashboardComponent} from '../dashboard/dashboard.component';
-import {AccueilComponent} from '../dashboard/accueil/accueil.component';
-import {MagasinComponent} from '../dashboard/magasin/magasin.component';
-import {NouvelleDemandeComponent} from '../dashboard/nouvelle-demande/nouvelle-demande.component';
-import {AproposComponent} from '../dashboard/apropos/apropos.component';
-import {HistoriqueDemandeComponent} from '../dashboard/historique-demande/historique-demande.component';
+import {DashboardComponent} from '../dashboard.component';
+import {AccueilComponent} from '../accueil/accueil.component';
+import {MagasinComponent} from '../magasin/magasin.component';
+import {NouvelleDemandeComponent} from '../nouvelle-demande/nouvelle-demande.component';
+import {AproposComponent} from '../apropos/apropos.component';
+import {HistoriqueDemandeComponent} from '../historique-demande/historique-demande.component';
 import {UtilisateursComponent} from './utilisateurs.component';
 import {ProfilComponent} from './profil/profil.component';
 import {ServiceComponent} from './service/service.component';
