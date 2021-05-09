31b75d4518985a976fd0e6ad0d9065e51f13b4a6
skilad <skilad4@gmail.com>
2021-05-09 16:11:46 +0100

    quelques modifications

--- src/app/dashboard/utilisateurs/utilisateurs.module.ts
+++ src/app/dashboard/utilisateurs/utilisateurs.module.ts
@@ -6,13 +6,50 @@ import { UtilisateursComponent } from './utilisateurs.component';
 import { ProfilComponent } from './profil/profil.component';
 import { ServiceComponent } from './service/service.component';
 import { UserRunComponent } from './user-run/user-run.component';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb';
+import {NzMenuModule} from 'ng-zorro-antd/menu';
+import {RouterModule} from '@angular/router';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NzSpaceModule} from 'ng-zorro-antd/space';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzRadioModule} from 'ng-zorro-antd/radio';
+import {NzGridModule} from 'ng-zorro-antd/grid';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzSelectModule} from 'ng-zorro-antd/select';
+import {NzBadgeModule} from 'ng-zorro-antd/badge';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { SharingModule } from 'src/app/sharing/sharing.module';
+
 
 
 @NgModule({
   declarations: [UtilisateursComponent, ProfilComponent, ServiceComponent, UserRunComponent],
   imports: [
     CommonModule,
-    UtilisateursRoutingModule
+    UtilisateursRoutingModule,
+    NzLayoutModule,
+    NzIconModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+    RouterModule,
+    SharingModule,
+    NzButtonModule,
+    ReactiveFormsModule,
+    NzSpaceModule,
+    NzFormModule,
+    NzRadioModule,
+    NzGridModule,
+    NzInputModule,
+    NzTabsModule,
+    NzTableModule,
+    NzSelectModule,
+    NzBadgeModule,
+    NzPopconfirmModule,
   ]
 })
 export class UtilisateursModule { }
