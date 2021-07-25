afd812f741cddf19b94cdc20d56717e4e1525f04
marieloujo <detchenoujoan@gmail.com>
2021-07-25 23:06:21 +0100

    Export PDF and CSV

---
+++ src/app/models/historique/historique.model.ts
@@ -0,0 +1,11 @@
+export class Historique {
+
+    date_demande: string;
+    date_Validation: string;
+    numserie: string;
+    marque: string;
+    modele: string;
+    gamme: string;
+    demandeur: string;
+
+}
